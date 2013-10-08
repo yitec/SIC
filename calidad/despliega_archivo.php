a34ad47f03d99ac27b20719ef515e4c0f1a31197
sergio <sbarrantes@yitec.net>
2013-10-07 22:45:59 -0600

    update despliega_archivos

---
+++ calidad/despliega_archivo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel ="stylesheet" href="../css/calidad.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css' />
+        <title>SIC CINA</title>
+    </head>
+    <body >
+    		<div class="header" ></div>
+    		<div class="box">
+            <div align="center">
+            <br>
+                <iframe width="900" height="800" src="https://docs.google.com/spreadsheet/ccc?key=0AtqAbbt-nkVPdHd0cEZrTU5fRFZiV0VtbEdkWkF1dWc&usp=sharing"></iframe>
+            </br>
+            </div>
+           
+			</div><!--Fin Box-->		
+    </body>
+</html>
+
