163bb15d9cf1aae9ce8f41dbe59675a24e649a0a
sergio <sbarrantes@yitec.net>
2017-11-01 22:04:13 -0600

    commit 1/11/2017

--- busca_informe.php
+++ busca_informe.php
@@ -174,11 +174,52 @@ if($_REQUEST['estado']==1){
     
 //2016
 if($_REQUEST['estado']==1){
-        $result=mysql_query("select * from tbl_contratos where  fecha_ingreso>='20160101' and (estado=1 or estado=2)  order by id ASC");    
+        $result=mysql_query("select * from tbl_contratos where  fecha_ingreso>='20160101' and  fecha_ingreso<='20161231' and (estado=1 or estado=2)  order by id ASC");    
     }else{
-        $result=mysql_query("select * from tbl_contratos where estado='"."4"."' and fecha_ingreso>='20160101' order by id ASC");    
+        $result=mysql_query("select * from tbl_contratos where estado='"."4"."' and fecha_ingreso>='20160101' and  fecha_ingreso<='20161231' order by id ASC");    
     }    
     echo '<div align="center" id="year_2016">    
+    <table width="747" height="18" border="1"   cellpadding="0" cellspacing="0" bordercolor="#a6c9e2">
+    <tr>
+    <td><div align="center" class="Arial14Azul">Contrato</div></td>
+    <td><div align="center" class="Arial14Azul">Muestras</div></td>    
+    <td><div align="center" class="Arial14Azul">Fecha Ingreso</div></td>
+    <td><div align="center" class="Arial14Azul">Ver Contrato</div></td>        
+    </tr>';
+    while ($row=mysql_fetch_assoc($result)){
+        if($row['id']>3161){
+            echo '<tr>
+    <td><div align="center" class="Arial14Negro">'.$row['consecutivo'].'</div></td>
+    <td><div align="center" class="Arial14Negro">'.$row['numero_muestras'].'</div></td>
+    <td><div align="center" class="Arial14Negro">'.$row['fecha_ingreso'].'</div></td>
+    <td><div align="center" class="Arial14Negro"><a href="nuevo_informe.php?id='.$row['id'].'" target="_blank"><img src="img/search.png" /></a>
+    </div></td>        
+    </tr>';
+
+        }else{
+        echo '<tr>
+    <td><div align="center" class="Arial14Negro">'.$row['consecutivo'].'</div></td>
+    <td><div align="center" class="Arial14Negro">'.$row['numero_muestras'].'</div></td>
+    <td><div align="center" class="Arial14Negro">'.$row['fecha_ingreso'].'</div></td>
+    <td><div align="center" class="Arial14Negro"><a href="informe.php?id='.$row['id'].'" target="_blank"><img src="img/search.png" /></a>
+    </div></td>        
+    </tr>';
+        }
+    }
+    echo '</table>
+    </div>';  
+    echo '<div align="center" class="Arial14Morado">Año 2016</div>';   
+    echo '<div align="center" class="Arial14Negro"><img id="btn_2016" src="img/search.png" />
+    </div><br>';   
+
+
+//2017
+if($_REQUEST['estado']==1){
+        $result=mysql_query("select * from tbl_contratos where  fecha_ingreso>='20170101' and (estado=1 or estado=2)  order by id ASC");    
+    }else{
+        $result=mysql_query("select * from tbl_contratos where estado='"."4"."' and fecha_ingreso>='20170101' order by id ASC");    
+    }    
+    echo '<div align="center" id="year_2017">    
     <table width="747" height="18" border="1"   cellpadding="0" cellspacing="0" bordercolor="#a6c9e2">
     <tr>
     <td><div align="center" class="Arial14Azul">Contrato</div></td>
--- includes/Scripts_Years.js
+++ includes/Scripts_Years.js
@@ -5,10 +5,12 @@ $("#year_2012").hide();
 $("#year_2013").hide();
 $("#year_2014").hide();
 $("#year_2015").hide();
+$("#year_2016").hide();
 $('#btn_2012').css('cursor','pointer');
 $('#btn_2013').css('cursor','pointer');
 $('#btn_2014').css('cursor','pointer');
 $('#btn_2015').css('cursor','pointer');
+$('#btn_2016').css('cursor','pointer');
 	
 $("#btn_2012").click(function() {
     var $this = $(this);
@@ -67,6 +69,21 @@ $("#btn_2015").click(function(event){
         // first time this is clicked, mark it
         $this.addClass("clicked-once");        
         $("#year_2015").show();
+    }
+});
+
+$("#btn_2016").click(function(event){
+    var $this = $(this);
+    if ($this.hasClass("clicked-once")) {
+        // already been clicked once, hide it
+        //$this.hide();
+        $("#year_2016").hide();
+        $this.removeClass("clicked-once")
+    }
+    else {
+        // first time this is clicked, mark it
+        $this.addClass("clicked-once");        
+        $("#year_2016").show();
     }
 }); 
 
